bf33e686132dad813e645012758b768704ef0aae
RuoChen95 <xrc_1995@163.com>
2019-04-08 11:53:30 +0800

    简历初稿完成

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <section>
-      <div class="header">
+    <section id="header">
+      <div class="left">
         <p class="name">解若琛</p>
         <p class="place">浙江 杭州 | 计算机软件</p>
+      </div>
+      <div class="right">
         <p class="education">教育背景：华北电力大学</p>
+        <p class="connect">
+          电话：17600370962
+          <br>
+          邮箱：xrc_1995@163.com
+        </p>
       </div>
     </section>
     <section id="self-intro">
       <p class="title">个人简介</p>
       <p class="intro">
-        在比特大陆工作两年后，目前我有更大的理想希望能学习更加广泛更加具有影响力。所以我现在正在coursera上学习算法，在udacity上学习全栈开发。除此之外，我也正在计划于2020年去香港/新加坡进一步拓展自己的学历。在未来，我希望能够建造出属于自己的网络应用，类似于notion以及transfix，使用最先进的技术，来让人们更为高效的工作。
+        在比特大陆工作两年后，目前我有更大的理想希望能学习更加广泛更加具有影响力。所以我现在正在coursera上学习算法，并完成了udacity上的全栈开发课程。除此之外，我也计划于2020年去香港/新加坡进一步拓展自己的学历。在未来，我希望能够建造出属于自己的网络应用，类似于notion以及transfix，使用最先进的技术，来让人们更为高效的工作。目前希望在工作中锻炼自己的实践技能。<br>技能：JavaScript, Python, Flask, Node.js, Vue.js, SQL, HTML, CSS, jQuery, Boostrap
       </p>
     </section>
     <section id="work-intro">
@@ -21,11 +28,20 @@
           <div class="des">
             <span class="title">Web前端工程师</span>
             <span class="company">Bitmain</span>
-            <span class="time">2017 年 4 月 – 2018 年 12 月 · 1 年 9 个月</span>
+            <span class="time">2017 年 4 月 – 2018 年 1 月 · 1 年 10 个月</span>
             <span class="location">中国 北京</span>
-            <span class="description">
-              工作内容主要是公司官网商城的单页面重构工作，具体包括了商品展示以及推广项目、下单流程项目、个人中心以及售后服务项目以及H5移动端项目。在项目的进行过程中，我学到了如何使用单页面框架Vue.js构建可拓展的网页应用，如何和后端、UI以及产品共同工作以及使用jira进行敏捷开发。
-            </span>
+            <div class="description">
+              <p>工作内容主要是公司官网商城的单页面重构工作以及旧有JSP项目的维护工作，具体包括了：</p>
+              <ul>
+                <li>售后服务项目：单页面框架的学习（Vue.js以及配套的UI框架elementUI）、单点登录、项目部署、简单的表格显示</li>
+                <li>下单流程：购物车页面的交互（包括步进器以及选择框联动）、订单结算页的选择优惠券（包括频控）、支付方式选择页面以及订单完成页</li>
+                <li>首页：包括推广页、首页和产品详情页</li>
+                <li>H5页面：官网整体流程，快速开发，手机适配</li>
+                <li>个人中心：和H5的个人中心同步开发，主要是列表的级联显示、数据的缓存以及不同情况下订单详情的显示（未支付等状态）、对接上传图片功能</li>
+                <li>售后微信小程序：微信对接以及页面的简单交互</li>
+              </ul>
+              <p>在项目的进行过程中，我学到了如何使用单页面框架Vue.js构建可拓展的网页应用，如何和后端、UI以及产品共同工作以及使用jira进行敏捷开发。</p>
+            </div>
           </div>
         </div>
       </div>
@@ -38,12 +54,13 @@
           <p class="content">工学学士，环境科学专业，top20%</p>
           <p class="time">2013年 - 2017年</p>
           <p class="activity">社团活动：干事，学生会，华北电力大学，2013-2014</p>
+          <p class="honor">荣誉：2013-2014 华北电力大学优秀学生</p>
         </div>
         <div><img src="../assets/education_logo_1.jpeg"></div>
       </div>
     </section>
     <section id="quantification-intro">
-      <p class="title">资格认证</p>
+      <p class="title">个人成就</p>
       <div class="qua">
         <div>
           <p class="name">Full Stack Web Developer Nanodegree</p>
@@ -53,10 +70,14 @@
           </p>
           <p class="time">2019 年</p>
           <div class="project-list">
-            <p class="subTitle">作品文件</p>
-            <p>logs analysis: 使用了内部数据库信息来发现读者喜欢什么种类的文章</p>
-            <p>item catalog: 一个简单网页服务，具有对于饭店以及其菜单的增删改查功能，用到了flask以及sqlAlchemy</p>
-            <p>linux server configuration: 基本的linux服务器配置，使用了Apache、wsgi以及Flask</p>
+            <p class="subTitle">作品文件：</p>
+            <p>Logs analysis: 使用了内部数据库信息来发现读者喜欢什么种类的文章</p>
+            <p>Item catalog: 具有对于饭店列表、菜单列表的增删改查功能和连接了外部登录API，用到了Flask以及SqlAlchemy</p>
+            <p>Linux server configuration: 基本的linux服务器配置，使用了Apache、wsgi以及Flask</p>
+          </div>
+          <div class="link-list">
+            <p class="subTitle">相关链接：</p>
+            <p>https://confirm.udacity.com/4TT6CGMD</p>
           </div>
         </div>
         <div><img src="../assets/quantification_logo_1.png"></div>
@@ -71,10 +92,15 @@
           </p>
           <p class="time">2019 年</p>
           <div class="project-list">
-            <p class="subTitle">作品文件</p>
-            <p>divide-conquer: 分治算法：数组的倒置量、Karatsuba乘法；随机算法：最小切割量</p>
-            <p>graphs-data-structures: 图搜索、最短路径算法、堆以及哈希表</p>
-            <p>greedy: 贪心算法、最小生成树算法以及动态规划</p>
+            <p class="subTitle">学习内容：</p>
+            <p>分治算法：归并排序、快速排序、数组的倒置量、Karatsuba乘法；随机算法：图的最小切割量</p>
+            <p>图搜索、最短路径算法、堆以及哈希表</p>
+            <p>贪心算法、最小生成树算法以及动态规划</p>
+          </div>
+          <div class="link-list">
+            <p class="subTitle">相关链接：</p>
+            <p>https://www.coursera.org/account/accomplishments/verify/K2EVGB8ZH6BG</p>
+            <p>https://www.coursera.org/account/accomplishments/verify/Q8KEBDEUEV4K</p>
           </div>
         </div>
         <div><img src="../assets/quantification_logo_2.jpeg"></div>
@@ -82,12 +108,35 @@
 
       <div class="qua">
         <div>
+          <p class="name">Front End Web Developer Nanodegree</p>
+          <p>
+            <span class="authority">Udacity</span>
+          </p>
+          <p class="time">2016 年</p>
+          <div class="project-list">
+            <p class="subTitle">作品文件：</p>
+            <p>Resume: 使用了面相对象编程思想，将数据进行封装后进行页面的显示。</p>
+            <p>Arcade Game: 实现了比较复杂的网页交互游戏，借助面相对象思想，通过继承和委托来构建合理的程序。</p>
+            <p>Website Optimization: 优化网页性能，使其达到目标的 PageSpeed 分数，并以 60 帧/秒的速度运行。</p>
+          </div>
+        </div>
+        <div><img src="../assets/quantification_logo_1.png"></div>
+      </div>
+
+      <div class="qua">
+        <div>
           <p class="name">Front end Certification</p>
           <p>
             <span class="authority">Freecodecamp, </span>
-            <span class="code">证书编号: https://freecodecamp.cn/ruochen95</span>
+            <span class="code">证书编号: https://www.freecodecamp.cn/ruochen95</span>
           </p>
           <p class="time">2016 年</p>
+          <div class="project-list">
+            <p class="subTitle">作品文件</p>
+            <p>Build a Wikipedia Viewer: 接收用户数据，后请求维基百科官方RESTful API并动态展示到页面上</p>
+            <p>Build a Pomodoro Clock: 番茄工作时钟的页面显示，使用了setInterval函数，具有提醒和统计的功能</p>
+            <p>Build a Simon Game: 借助用户监听模拟了Simon Game：色块随机亮起，并能记录以及响应用户点击，如果用户点击正确，会延长随机序列，如果用户点击错误，会有提示。</p>
+          </div>
         </div>
         <div><img src="../assets/quantification_logo_3.png"></div>
       </div>
@@ -108,114 +157,176 @@ export default {
 };
 </script>
 <style lang="less">
-section {
-  box-shadow:
-      0 1px 1px rgba(0,0,0,0.15),
-      -1px 0 0 rgba(0,0,0,0.03),
-      1px 0 0 rgba(0,0,0,0.03),
-      0 1px 0 rgba(0,0,0,0.12);
-  border: 1px solid rgba(0,0,0,0.12);
-  padding: 30px 35px;
-  background: white;
-  margin-bottom: 10px;
+  section {
+    box-shadow:
+        0 1px 1px rgba(0,0,0,0.15),
+        -1px 0 0 rgba(0,0,0,0.03),
+        1px 0 0 rgba(0,0,0,0.03),
+        0 1px 0 rgba(0,0,0,0.12);
+    border: 1px solid rgba(0,0,0,0.12);
+    padding: 30px 35px;
+    background: white;
+    margin-bottom: 10px;
 
-  text-align: left;
-}
-div.header {
-  display: flex;
-  flex-direction: column;
-  justify-content: flex-start;
-  align-items: flex-start;
-  p {
-    margin: 0;
-  }
-  p.name {
-    font-size: 26px;
-    font-weight: bold;
-    line-height: 30px;
-    color: #000;
-  }
-  p.place {
-    display: inline-block;
-    margin-top: 5px;
-    font-size: 13px;
-    line-height: 17px;
-    color: #66696a;
-  }
-  p.education {
-    margin-top: 10px;
-    display: inline;
-    font-size: 12px;
-    line-height: 14px;
-    color: #333;
-    font-weight: normal;
-  }
-}
-section {
-  p.title {
-    margin-bottom: 30px;
-    font-size: 16px;
-    font-weight: normal;
-    line-height: 18px;
-    color: #434649;
-  }
-}
-
-section#self-intro {
-  p.intro {
-    font-size: 14px;
-  }
-}
-section#work-intro {
-  div.logo {
-    padding: 4px;
-    margin-right: 8px;
-    img {
-      max-width: 60px;
-      max-height: 60px;
+    text-align: left;
+    p.title {
+      margin-bottom: 30px;
+      font-size: 16px;
+      font-weight: normal;
+      line-height: 18px;
+      color: #434649;
     }
   }
-  div.works {
+  section#header {
     display: flex;
-    div.logo {}
-    div.des {
+    flex-direction: row;
+    align-items: center;
+    justify-content: space-between;
+    p {
+      margin: 0;
+    }
+    div.left {
       display: flex;
       flex-direction: column;
-      font-size: 14px;
-      span.title {
-        font-size: 16px;
+      justify-content: flex-start;
+      align-items: flex-start;
+      p.name {
+        font-size: 26px;
         font-weight: bold;
+        line-height: 30px;
+        color: #000;
       }
-      span.company{}
-      span.time{}
-      span.location {
-        color: rgba(0,0,0,0.6);
-      }
-      span.description {
+      p.place {
         display: inline-block;
-        margin-top: 8px;
+        margin-top: 5px;
+        font-size: 13px;
+        line-height: 17px;
+        color: #66696a;
+      }
+    }
+    div.right {
+      display: flex;
+      flex-direction: column;
+      justify-content: flex-start;
+      align-items: flex-start;
+      p.education {
+        margin-top: 10px;
+        display: inline;
+        font-size: 12px;
+        line-height: 14px;
+        color: #333;
+        font-weight: normal;
+      }
+      p.connect {
+        margin-top: 10px;
+        display: inline;
+        font-size: 12px;
+        line-height: 14px;
+        color: #66696a;
+        font-weight: normal;
       }
     }
   }
-}
-section#education-intro {
-  div.edu {
-    display: flex;
-    justify-content: space-between;
-    p.place{
+  section#self-intro {
+    p.intro {
+      font-size: 14px;
+    }
+  }
+  section#work-intro {
+    div.logo {
+      padding: 4px;
+      margin-right: 8px;
+      img {
+        max-width: 60px;
+        max-height: 60px;
+      }
+    }
+    div.works {
+      display: flex;
+      div.logo {}
+      div.des {
+        display: flex;
+        flex-direction: column;
+        font-size: 14px;
+        span.title {
+          font-size: 16px;
+          font-weight: bold;
+        }
+        span.company{}
+        span.time{}
+        span.location {
+          color: rgba(0,0,0,0.6);
+        }
+        div.description {
+          display: inline-block;
+          margin-top: 8px;
+        }
+      }
+    }
+  }
+  section#education-intro {
+    div.edu {
+      display: flex;
+      justify-content: space-between;
+      p.place{
+        font-size: 16px;
+        font-weight: bold;
+        line-height: 18px;
+        color: #000;
+
+        margin-bottom: 5px;
+      }
+      p.content{
+        font-size: 14px;
+        font-weight: normal;
+        line-height: 16px;
+        color: #434649;
+
+        margin-bottom: 5px;
+      }
+      p.time{
+        font-size: 12px;
+        line-height: 14px;
+        color: #66696a;
+      }
+      p.activity{
+        font-size: 13px;
+        line-height: 17px;
+        color: #333;
+        font-weight: normal;
+        margin-top: 15px;
+      }
+      p.honor {
+        font-size: 13px;
+        line-height: 17px;
+        color: #333;
+        font-weight: normal;
+      }
+      img {
+        max-width: 60px;
+        max-height: 60px;
+      }
+    }
+  }
+  section#quantification-intro {
+    padding: 0;
+    font-size: 14px;
+    p.title {
+      padding: 30px 35px 0 35px;
+      margin: 0;
+    }
+    p.name {
       font-size: 16px;
       font-weight: bold;
       line-height: 18px;
       color: #000;
-
       margin-bottom: 5px;
     }
-    p.content{
+    p.code {
       font-size: 14px;
       font-weight: normal;
       line-height: 16px;
       color: #434649;
-
       margin-bottom: 5px;
     }
     p.time{
@@ -223,59 +334,21 @@ section#education-intro {
       line-height: 14px;
       color: #66696a;
     }
-    p.activity{
-      font-size: 13px;
-      line-height: 17px;
-      color: #333;
-      font-weight: normal;
-      margin-top: 15px;
-    }
-    img {
-      max-width: 60px;
-      max-height: 60px;
-    }
-  }
-}
-#quantification-intro {
-  padding: 0;
-  font-size: 14px;
-  p.title {
-    padding: 30px 35px 0 35px;
-    margin: 0;
-  }
-  p.name {
-    font-size: 16px;
-    font-weight: bold;
-    line-height: 18px;
-    color: #000;
-    margin-bottom: 5px;
-  }
-  p.code {
-    font-size: 14px;
-    font-weight: normal;
-    line-height: 16px;
-    color: #434649;
-    margin-bottom: 5px;
-  }
-  p.time{
-    font-size: 12px;
-    line-height: 14px;
-    color: #66696a;
-  }
-  div.qua {
-    display: flex;
-    justify-content: space-between;
-    padding: 30px 35px;
+    div.qua {
+      display: flex;
+      justify-content: space-between;
+      padding: 30px 35px;
 
-    border-bottom: 1px solid #ddd;
-  img {
-      max-width: 60px;
-      max-height: 60px;
+      border-bottom: 1px solid #ddd;
+    img {
+        max-width: 60px;
+        max-height: 60px;
+      }
+    }
+    div.project-list,
+    div.link-list{
+      margin-top: 15px;
+      font-size: 13px;
     }
   }
-  div.project-list {
-    margin-top: 15px;
-    font-size: 13px;
-  }
-}
 </style>
